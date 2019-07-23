582fb1897428b36fe4f22d748ee4ced05e8ae6a0
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2019-07-23 21:45:30 +0200

    add pointer to function

--- examples/test.c
+++ examples/test.c
@@ -1,3 +1,11 @@
-struct test_struct { int test; int truc; };
+typedef int (*foo_cb)(int a, int b);
 
-static const struct test_struct decl_var_struct = { .test = 42, .truc = 72 };
+struct test_struct { int test; int truc; foo_cb *pf_foo; };
+
+int foo(int a, int b) { return a + b; }
+
+static const struct test_struct decl_var_struct = {
+    .test = 42,
+    .truc = 72,
+    .pf_foo = foo,
+};
